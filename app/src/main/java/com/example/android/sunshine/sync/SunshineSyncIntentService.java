8d15b51d7a33583a90401e7b3e589ef6a4afb38b
Csegota <csegota406@gmail.com>
2019-06-24 18:01:08 -0700

    Better Weather Data Sync

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -18,7 +18,6 @@ package com.example.android.sunshine.sync;
 import android.app.IntentService;
 import android.content.Intent;
 
-// COMPLETED (5) Create a new class called SunshineSyncIntentService that extends IntentService
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -26,12 +25,10 @@ import android.content.Intent;
  */
 public class SunshineSyncIntentService extends IntentService {
 
-    //  COMPLETED (6) Create a constructor that calls super and passes the name of this class
     public SunshineSyncIntentService() {
         super("SunshineSyncIntentService");
     }
 
-    //  COMPLETED (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
     @Override
     protected void onHandleIntent(Intent intent) {
         SunshineSyncTask.syncWeather(this);
